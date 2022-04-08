57b0b586f75b04e8f0f0bc1ec5f24c635f1fe385
Matheus Santos <98832555+santos95mat@users.noreply.github.com>
2022-04-08 15:38:15 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,18 @@
 const button = document.querySelector(".button");
 
-let faseA = 0;
-let faseN = 1;
+let fase = 1;
 let frases = ["Goku", "Goku SSJ 1", "Goku SSJ 2", "Goku SSJ 3", "Goku SSJ 4", "Goku SSJ Deus", "Goku SSJ Azul", "Goku SSJ Superior"]
 
 button.addEventListener("click", function() {
-    if(faseA > 7) {
-        faseA = 0;
+    if(fase > 7) {
+        fase = 0;
     }
 
-    if(faseN > 7) {
-        faseN = 0;
-    }
+    const goku = "#goku"+fase;
 
-    const gokuA = "#goku"+faseA;
-    const gokuN = "#goku"+faseN;
+    const estado = document.querySelector(".goku").id = goku;
 
-    const estadoA = document.querySelector(gokuA).style.display = "none";
-    const estadoN = document.querySelector(gokuN).style.display = "block";
+    button.value = frases[fase];
 
-    button.value = frases[faseN];
-
-    faseA++;
-    faseN++;
+    fase++;
 });
