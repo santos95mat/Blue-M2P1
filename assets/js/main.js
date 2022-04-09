136e9a046f981462c9eed47345e1f622eab1713c
santos95mat <santos95.mat@gmail.com>
2022-04-09 00:00:13 -0300

    Modifica texto btn

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,9 +2,16 @@ const button = document.querySelector(".button");
 const text = document.querySelector(".text")
 
 let fase = 1;
-let frases = ["Normal", "SSJ 1", "SSJ 2", "SSJ 3", "SSJ 4", "Deus", "Deus azul", "Instinto Superior"]
-
+const frases = ["Normal", "SSJ 1", "SSJ 2", "SSJ 3", "SSJ 4", "Deus", "Deus azul", "Instinto Superior"];
+const btn = ["Zerar", "Evoluir"];
+ 
 button.addEventListener("click", function() {
+    if(fase === 7) {
+        button.value = btn[0];
+    } else {
+        button.value = btn[1];
+    }
+
     if(fase > 7) {
         fase = 0;
     }
